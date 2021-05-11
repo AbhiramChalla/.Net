e40038263644ebddec0703c9fa96d6444ac28bd5
ABHIRAM\hp <challa.abhiram827@gmail.com>
2021-05-11 19:06:52 -0400

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a=10;
+            int a = 0;
+            int b = 0;
+            int sum = 0;
+            Console.WriteLine("Enter value for a: ");
+            Console.ReadLine();
+            Console.WriteLine("Enter value for b: ");
+            Console.ReadLine();
+            sum = Add(a, b);
+
+        }
+
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
